66eb561a832523fba7a995caa36f5c0c200632ed
Saira Haider <shaider@usgs.gov>
2018-01-23 11:03:07 -0500

    Reverting back to Trial 5 & adding L67 extension

--- Scripts/eden_v3.R
+++ Scripts/eden_v3.R
@@ -9,9 +9,11 @@ library(raster)
 library(rgdal)
 library(geoR)
 
+options(digits = 15)
+
 nad_utm <- CRS("+proj=utm +zone=17 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
-## read in stage values at gages & convert to sp* ojbect ----
+## read in stage values at gages ----
 
 # unaltered text file of daily means downloaded from EDEN website
 gages <- read.table("E:/altEden/gage_data/2008_q4_DM_v2r1/20081001_median.txt", 
@@ -23,6 +25,29 @@ colnames(gages)[4] <- "stage_cm"
 # fix incorrect location of gauge G-3567
 gages$X[gages$Station == "G-3567"] <- 556509
 gages$Y[gages$Station == "G-3567"] <- 2864737
+
+
+## add in points to pseudo-replicate the L67-Extension canal ----
+# which is located in 'other'
+
+pt1 <- gages$stage_cm[gages$Station == "S333+H"] - 23.0509
+pt2 <- gages$stage_cm[gages$Station == "S333+H"] - 43.92989
+pt3 <- gages$stage_cm[gages$Station == "S333+H"] - 63.20402
+
+pt4 <- gages$stage_cm[gages$Station == "S333+T"] - 1.2068
+pt5 <- gages$stage_cm[gages$Station == "S333+T"] + 1.0407
+pt6 <- gages$stage_cm[gages$Station == "S333+T"] + 3.098
+
+gages[nrow(gages) + 1, ] <- list("l67ext_pt1", 532760.759, 2846703.198, pt1)
+gages[nrow(gages) + 1, ] <- list("l67ext_pt2", 532851.2435, 2844104.773, pt2)
+gages[nrow(gages) + 1, ] <- list("l67ext_pt3", 532934.7678, 2841706.227, pt3)
+
+gages[nrow(gages) + 1, ] <- list("l67ext_pt4", 532810.759, 2846703.198, pt4)
+gages[nrow(gages) + 1, ] <- list("l67ext_pt5", 532901.2435, 2844104.773, pt5)
+gages[nrow(gages) + 1, ] <- list("l67ext_pt6", 532984.7678, 2841706.227, pt6)
+
+
+## convert to sp* ojbect ----
 
 # convert to sp points df
 coordinates(gages) <- ~ X + Y
@@ -69,6 +94,8 @@ subgages <- na.omit(subgages)
 other_gages <- gages[row.names(subgages), ]
 other_gages@data 
 
+plot(other_gages, pch = 1, cex = other_gages$stage_cm/200)
+
 ## create dataframes of the polygon centroids ----
 # and change column names to match the gage spdf
 # which is required for rbf
@@ -86,58 +113,38 @@ colnames(other_coords) <- c("X", "Y")
 
 ##  perform rbf interpolation ----
 
-# optimize rbf parameters eta and rho & run rbf
+# run rbf
 # add output of predicted stage values to df of original coordinates
-rbf_param <- graph.rbf(stage_cm ~ X + Y, data = wca1_gages, 
-                       eta.opt = TRUE, rho.opt = TRUE, iter = 120, n.neigh = 8, 
-                       func = "M", rho.dmax = 400, eta.dmax = 400)
 wca1_rbf <- rbf(stage_cm ~ X + Y, data = wca1_gages, func = "M", 
-                eta = rbf_param$Opt$par[1], rho = rbf_param$Opt$par[2],
-                n.neigh = 8, newdata = wca1_coords) 
+                eta = 0, rho = 0, n.neigh = 8, newdata = wca1_coords) 
 wca1_rbf <- subset(wca1_rbf, select = -var1.var)
 colnames(wca1_rbf)[3] <- "alt_stage_cm"
 head(wca1_rbf)
 
 
-rbf_param <- graph.rbf(stage_cm ~ X + Y, data = wca2b_gages, 
-                       eta.opt = TRUE, rho.opt = TRUE, iter = 120, n.neigh = 7, 
-                       func = "M", rho.dmax = 400, eta.dmax = 400)
 wca2b_rbf <- rbf(stage_cm ~ X + Y, data = wca2b_gages, func = "M", 
-                 eta = rbf_param$Opt$par[1], rho = rbf_param$Opt$par[2],
-                 n.neigh = 8, newdata = wca2b_coords) 
+                eta = 0, rho = 0, n.neigh = 8, newdata = wca2b_coords) 
 wca2b_rbf <- subset(wca2b_rbf, select = -var1.var)
 colnames(wca2b_rbf)[3] <- "alt_stage_cm"
 head(wca2b_rbf)
 
 
-rbf_param <- graph.rbf(stage_cm ~ X + Y, data = wca3b_gages, 
-                       eta.opt = TRUE, rho.opt = TRUE, iter = 120, n.neigh = 8, 
-                       func = "M", rho.dmax = 400, eta.dmax = 400)
 wca3b_rbf <- rbf(stage_cm ~ X + Y, data = wca3b_gages, func = "M", 
-                 eta = rbf_param$Opt$par[1], rho = rbf_param$Opt$par[2],
-                 n.neigh = 8, newdata = wca3b_coords) 
+                eta = 0, rho = 0, n.neigh = 8, newdata = wca3b_coords) 
 wca3b_rbf <- subset(wca3b_rbf, select = -var1.var)
 colnames(wca3b_rbf)[3] <- "alt_stage_cm"
 head(wca3b_rbf)
 
 # note the number of neighbors here
-rbf_param <- graph.rbf(stage_cm ~ X + Y, data = pw_gages, 
-                       eta.opt = TRUE, rho.opt = TRUE, iter = 120, n.neigh = 4, 
-                       func = "M", rho.dmax = 400, eta.dmax = 400)
 pw_rbf <- rbf(stage_cm ~ X + Y, data = pw_gages, func = "M", 
-              eta = rbf_param$Opt$par[1], rho = rbf_param$Opt$par[2],
-              n.neigh = 5, newdata = pw_coords) 
+                eta = 0, rho = 0, n.neigh = 5, newdata = pw_coords) 
 pw_rbf <- subset(pw_rbf, select = -var1.var)
 colnames(pw_rbf)[3] <- "alt_stage_cm"
 head(pw_rbf)
 
 
-rbf_param <- graph.rbf(stage_cm ~ X + Y, data = other_gages, 
-                       eta.opt = TRUE, rho.opt = TRUE, iter = 120, n.neigh = 8, 
-                       func = "M", rho.dmax = 400, eta.dmax = 400)
 other_rbf <- rbf(stage_cm ~ X + Y, data = other_gages, func = "M", 
-                 eta = rbf_param$Opt$par[1], rho = rbf_param$Opt$par[2],
-                 n.neigh = 8, newdata = other_coords) 
+                eta = 0, rho = 0, n.neigh = 8, newdata = other_coords) 
 other_rbf <- subset(other_rbf, select = -var1.var)
 colnames(other_rbf)[3] <- "alt_stage_cm"
 head(other_rbf)
@@ -173,5 +180,5 @@ diff <- na.omit(getValues(eden_diff))
 sqrt(mean(diff^2))
 
 ## export ----
-writeRaster(alt_eden, "E:/altEden/R_run2/output/altEden_Trial6_20081001.tif")
-writeRaster(eden_diff, "E:/altEden/R_run2/output/edenDiff_Trial6_20081001.tif")
+writeRaster(alt_eden, "E:/altEden/R_run2/output/altEden_Trial5_l67ext_20081001.tif")
+writeRaster(eden_diff, "E:/altEden/R_run2/output/edenDiff_Trial5_l67ext_20081001.tif")
