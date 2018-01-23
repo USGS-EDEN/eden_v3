e06afd201ecbdbb91fe0ec80005961fbab295d49
Saira Haider <shaider@usgs.gov>
2018-01-23 10:42:57 -0500

    Run 2 Trial 1
    run2 refers to effort to replicate EDEN surface interpolation after having official EDEN documentation concerning the parameters of the RBF function

--- Scripts/eden_v3.R
+++ Scripts/eden_v3.R
@@ -1,211 +1,211 @@
 #-------------------------------------------------------
-# NOTES AND QUESTIONS TO BE ADDRESSED: !!!!!!!!
-# - try more experimenting with eta and rho
-# - leonard's note about the levee extension
-# fix so that defns and file locations is more proper?
-# what about that section to the east of WCA3B 
-# - can that be made into its own polygon?
-
-#-------------------------------------------------------
-# REPRODUCTION OF EDEN SURFACE INTERPOLATION USING RBF
+# REPRODUCTION OF EDEN SURFACE INTERPOLATION
+# TRIAL 1: WITH ANISOTROPY ANGLE & RATIO, ETA = 16.77, RHO = 0
 #-------------------------------------------------------
 
-library(sp)
 library(geospt)
-library(dplyr)
+#library(dplyr)
 library(raster)
+library(rgdal)
+library(geoR)
 
-#-------------------------------------------------------
-# DEFINTIONS AND FILES
+nad_utm <- CRS("+proj=utm +zone=17 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0")
 
-# working directory
-home_folder <- "E:/altEden/R_run1"
+## read in stage values at gages & convert to sp* ojbect ----
 
-# input files:
 # unaltered text file of daily means downloaded from EDEN website
-gauge_df <- read.table("E:/altEden/gage_data/2008_q4_DM_v2r1/20081001_median.txt", 
-                        header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-
-# USGS EDEN interpolation file
-eden_tiff <- "E:/altEden/EDEN_surfaces/2008_q4_tiff_v2r1/s_20081001.tif"
-
-# csv of eden grid, includes region location for each grid cell
-edenFile <- "E:/altEden/GIS_run1/EDEN_grid_pts.csv"
-
-# csv that relates the gauge station to region location
-station_id <- read.csv("E:/altEden/R_run1/station_region_id.csv", 
-                       header = TRUE, stringsAsFactors = FALSE)
-
-# output file
-# format: rbf_output_#nearestneighbors_yearmonthday.csv
-rbf_output <- "altEDEN_8n_20081001"
-
-# RBF Parameters
-p_neigh <- 8
-p_rho <- 0
-p_eta <- 0
-
-#-------------------------------------------------------
-# PREPARE GAUGE DAILY MEANS DATA
-
-# fix water stage column name
-# 5th column is the stage value from downloaded csvs
-colnames(gauge_df)[5] <- "stage_cm"
-head(gauge_df)
+gages <- read.table("E:/altEden/gage_data/2008_q4_DM_v2r1/20081001_median.txt", 
+                    header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+head(gages)
+gages <- gages[, c("Station", "X", "Y", "Daily.median.water.level..in.cm.")]
+colnames(gages)[4] <- "stage_cm"
 
 # fix incorrect location of gauge G-3567
-gauge_df$X[gauge_df$Station == "G-3567"] <- 556509
-gauge_df$Y[gauge_df$Station == "G-3567"] <- 2864737
+gages$X[gages$Station == "G-3567"] <- 556509
+gages$Y[gages$Station == "G-3567"] <- 2864737
 
-# add column for the regional location of each gauge
-gaugeMeans <- left_join(gauge_df, station_id, by = ("Station" = "Station"))
-head(gaugeMeans)
+# convert to sp points df
+coordinates(gages) <- ~ X + Y
+proj4string(gages) <- nad_utm
 
-# str(gaugeMeans)
-#options(digits = 10)
-#head(gaugeMeans)
+## read in polygon shapefiles of the 5 subzones ----
+wca1 <- readOGR(dsn = path.expand("E:/altEden/GIS_run2/EDEN_zones_GIS"),
+                layer = "EDEN_grid_poly_Jan_10_WCA1")
+wca2b <- readOGR(dsn = path.expand("E:/altEden/GIS_run2/EDEN_zones_GIS"),
+                layer = "EDEN_grid_poly_Jan_10_WCA2B")
+wca3b <- readOGR(dsn = path.expand("E:/altEden/GIS_run2/EDEN_zones_GIS"),
+                layer = "EDEN_grid_poly_Jan_10_WCA3B")
+pw <- readOGR(dsn = path.expand("E:/altEden/GIS_run2/EDEN_zones_GIS"),
+                layer = "EDEN_grid_poly_Jan_10_PW")
+other <- readOGR(dsn = path.expand("E:/altEden/GIS_run2/EDEN_zones_GIS"),
+                          layer = "EDENGRID_Jan_10_NO_WCA1_2B_3B_PW")
+head(wca2b)
+plot(pw)
 
-coordinates(gaugeMeans) <- ~ X + Y
+## find which gages are in which of the 5 subzones &  make subdivide the data ----
 
-# break into independent regions
-gauge_wca1a <- gaugeMeans[gaugeMeans$Region == "WCA1A", ]
-gauge_wca2a <- gaugeMeans[gaugeMeans$Region == "WCA2A", ]
-gauge_wca2b <- gaugeMeans[gaugeMeans$Region == "WCA2B", ]
-gauge_wca3a <- gaugeMeans[gaugeMeans$Region == "WCA3A", ]
-gauge_wca3b <- gaugeMeans[gaugeMeans$Region == "WCA3B", ]
-gauge_np <- gaugeMeans[gaugeMeans$Region == "NP", ]
+subgages <- over(gages, wca1) # find which pts are within the subzone
+subgages <- na.omit(subgages) # remove the pts which are not within the subzone
+wca1_gages <- gages[row.names(subgages), ] # subset original df by row index
+wca1_gages@data # check
+ 
+subgages <- over(gages, wca2b)
+subgages <- na.omit(subgages) 
+wca2b_gages <- gages[row.names(subgages), ]
+wca2b_gages@data 
 
+subgages <- over(gages, wca3b)
+subgages <- na.omit(subgages) 
+wca3b_gages <- gages[row.names(subgages), ]
+wca3b_gages@data 
 
-head(gauge_wca2a)
-gauge_wca1a@data
-#-------------------------------------------------------
-# PREPARE EDEN GRID DATA (FOR USE AS 'NEWDATA')
+subgages <- over(gages, pw)
+subgages <- na.omit(subgages) 
+pw_gages <- gages[row.names(subgages), ]
+pw_gages@data 
 
-edenGrid <- read.csv(edenFile, header = TRUE)
-head(edenGrid)
-names(edenGrid)[2:3] <- c("X", "Y") # must have same names!
+subgages <- over(gages, other)
+subgages <- na.omit(subgages) 
+other_gages <- gages[row.names(subgages), ]
+other_gages@data 
 
-coordinates(edenGrid) <- ~ X + Y
+## create dataframes of the polygon centroids ----
 
-# break into independent regions
-eden_wca1a <- edenGrid[edenGrid$Zone == "WCA1A", ]
-eden_wca2a <- edenGrid[edenGrid$Zone == "WCA2A", ]
-eden_wca2b <- edenGrid[edenGrid$Zone == "WCA2B", ]
-eden_wca3a <- edenGrid[edenGrid$Zone == "WCA3A", ]
-eden_wca3b <- edenGrid[edenGrid$Zone == "WCA3B", ]
-eden_np <- edenGrid[edenGrid$Zone == "NP", ]
-head(eden_np)
+wca1_coords <- wca1@data[, c("X_COORD", "Y_COORD")]
+wca2b_coords <- wca2b@data[, c("X_COORD", "Y_COORD")]
+wca3b_coords <- wca3b@data[, c("X_COORD", "Y_COORD")]
+pw_coords <- pw@data[, c("X_COORD", "Y_COORD")]
+other_coords <- other@data[, c("X_COORD", "Y_COORD")]
 
-#-------------------------------------------------------
-# first attempt at radial basis function
-# FIRST NEED:
-# 1) eta = optimal smoothing parameter
-# 2) rho = optimal robustness parameter
-# tried ranges 0-5 for eta and rho for wca1a, didn't seem to change anything
-# Arc's geostatistical wizard calculates "0" as the best optimization parameter
-# 3) n.neigh = no. of nearest observations used for predictions
-# seems like the more observations the smoother the plot
-# tried using 5, 8, and 10, 8 seemed to work best
+## convert to anisotropic coords ----
 
-# with each region
-wca1a.rbf <- rbf(stage_cm ~ X + Y, data = gauge_wca1a, func = "M", eta = p_eta, rho = p_rho, 
-                 n.neigh = p_neigh, newdata = eden_wca1a) 
-# show prediction map
-#coordinates(wca1a.rbf) <- ~ x + y
-#gridded(wca1a.rbf) <- TRUE
-#spplot(wca1a.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "wca1a Stage", key.space=list(space="right", cex=0.8))
+# convert both the gage & centroids coordinates
+# rename coordinates column names
+# add stage values back to gage df
+wca1_gages_anis <- as.data.frame(coords.aniso(coords = wca1_gages@coords, 
+                                aniso.pars = c(350, 31/30)))
+wca1_anis <- as.data.frame(coords.aniso(coords = wca1_coords, 
+                                        aniso.pars = c(350, 31/30)))
+colnames(wca1_gages_anis) <- c("x_aniso", "y_aniso")  
+colnames(wca1_anis) <- c("x_aniso", "y_aniso") 
+wca1_gages_anis$stage_cm <- wca1_gages$stage_cm
 
 
-wca2a.rbf <- rbf(stage_cm ~ X + Y, data = gauge_wca2a, func = "M", eta = p_eta, rho = p_rho, 
-                 n.neigh = p_neigh, newdata = eden_wca2a) 
-# show prediction map
-#coordinates(wca2a.rbf) <- ~ x + y
-#gridded(wca2a.rbf) <- TRUE
-#spplot(wca2a.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "wca2a Stage", key.space=list(space="right", cex=0.8))
+wca2b_gages_anis <- as.data.frame(coords.aniso(coords = wca2b_gages@coords, 
+                                               aniso.pars = c(350, 31/30)))
+wca2b_anis <- as.data.frame(coords.aniso(coords = wca2b_coords, 
+                                         aniso.pars = c(350, 31/30)))
+colnames(wca2b_gages_anis) <- c("x_aniso", "y_aniso")  
+colnames(wca2b_anis) <- c("x_aniso", "y_aniso")
+wca2b_gages_anis$stage_cm <- wca2b_gages$stage_cm
 
-wca2b.rbf <- rbf(stage_cm ~ X + Y, data = gauge_wca2b, func = "M", eta = p_eta, rho = p_rho, 
-                 n.neigh = p_neigh, newdata = eden_wca2b) 
-# show prediction map
-#coordinates(wca2b.rbf) <- ~ x + y
-#gridded(wca2b.rbf) <- TRUE
-#spplot(wca2b.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "wca2b Stage", key.space=list(space="right", cex=0.8))
 
-wca3a.rbf <- rbf(stage_cm ~ X + Y, data = gauge_wca3a, func = "M", eta = p_eta, rho = p_rho, 
-                 n.neigh = p_neigh, newdata = eden_wca3a) 
-# show prediction map
-#coordinates(wca3a.rbf) <- ~ x + y
-#gridded(wca3a.rbf) <- TRUE
-#spplot(wca3a.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "wca3a Stage", key.space=list(space="right", cex=0.8))
+wca3b_gages_anis <- as.data.frame(coords.aniso(coords = wca3b_gages@coords, 
+                                              aniso.pars = c(350, 31/30)))
+wca3b_anis <- as.data.frame(coords.aniso(coords = wca3b_coords, 
+                                        aniso.pars = c(350, 31/30)))
+colnames(wca3b_gages_anis) <- c("x_aniso", "y_aniso")  
+colnames(wca3b_anis) <- c("x_aniso", "y_aniso")
+wca3b_gages_anis$stage_cm <- wca3b_gages$stage_cm
 
-wca3b.rbf <- rbf(stage_cm ~ X + Y, data = gauge_wca3b, func = "M", eta = p_eta, rho = p_rho, 
-                 n.neigh = p_neigh, newdata = eden_wca3b) 
-# show prediction map
-#coordinates(wca3b.rbf) <- ~ x + y
-#gridded(wca3b.rbf) <- TRUE
-#spplot(wca3b.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "wca3b Stage", key.space=list(space="right", cex=0.8))
 
-np.rbf <- rbf(stage_cm ~ X + Y, data = gauge_np, func = "M", eta = p_eta, rho = p_rho, 
-              n.neigh = p_neigh, newdata = eden_np) 
-# show prediction map
-#coordinates(np.rbf) <- ~ x + y
-#gridded(np.rbf) <- TRUE
-#spplot(np.rbf["var1.pred"], cuts=40, col.regions=bpy.colors(100),
-#       main = "np Stage", key.space=list(space="right", cex=0.8))
+pw_gages_anis <- as.data.frame(coords.aniso(coords = pw_gages@coords, 
+                                              aniso.pars = c(350, 31/30)))
+pw_anis <- as.data.frame(coords.aniso(coords = pw_coords, 
+                                        aniso.pars = c(350, 31/30)))
+colnames(pw_gages_anis) <- c("x_aniso", "y_aniso")  
+colnames(pw_anis) <- c("x_aniso", "y_aniso")
+pw_gages_anis$stage_cm <- pw_gages$stage_cm
 
-#------------------------------------------------------------------------------
-# MERGE TOGETHER AND EXPORT
 
-eden_rbf <- rbind(np.rbf, wca1a.rbf, wca2a.rbf, wca2b.rbf, wca3a.rbf, wca3b.rbf)
+other_gages_anis <- as.data.frame(coords.aniso(coords = other_gages@coords, 
+                                              aniso.pars = c(350, 31/30)))
+other_anis <- as.data.frame(coords.aniso(coords = other_coords, 
+                                        aniso.pars = c(350, 31/30)))
+colnames(other_gages_anis) <- c("x_aniso", "y_aniso")  
+colnames(other_anis) <- c("x_aniso", "y_aniso")
+other_gages_anis$stage_cm <- other_gages$stage_cm
 
-# add cell id column
-eden_df <- read.csv(edenFile, header = TRUE)
-eden_rbf <- left_join(eden_rbf, eden_df, by = c("x" = "x_centroid", "y" = "y_centroid"))
-head(eden_rbf)
-nrow(eden_rbf)
-# can export eden_rbf here as data frame with EDEN cell id, if desired
-setwd(home_folder)
-file_name <- paste(rbf_output, "csv", sep = ".")
-write.csv(eden_rbf, file_name, row.names = FALSE)
 
-#------------------------------------------------------------------------------
-# CONVERT TO RASTER FORMAT
+##  perform rbf interpolation ----
 
-altEden <- eden_rbf
-coordinates(altEden) <- ~ x + y
-proj4string(altEden) <- CRS("+proj=utm +zone=17 +ellps=GRS80 +datum=NAD83 +units=m +no_defs")
-gridded(altEden) <- TRUE
-altEden <- raster(altEden)
-plot(altEden)
-altEden
+# convert gage data to spatialpointsdf for rbf
+# run rbf
+# add output of predicted stage values to df of original coordinates
+coordinates(wca1_gages_anis) <- ~x_aniso + y_aniso
+proj4string(wca1_gages_anis) <- nad_utm
+wca1_rbf <- rbf(stage_cm ~ x_aniso + y_aniso, data = wca1_gages_anis, func = "M", 
+                eta = 16.77, rho = 0, n.neigh = 8, newdata = wca1_anis) 
+wca1_rbf <- cbind(wca1_coords, wca1_rbf$var1.pred)
+colnames(wca1_rbf)[3] <- "alt_stage_cm"
+head(wca1_rbf)
 
-# export if desired
-setwd(home_folder)
-raster_name <- paste(rbf_output, "tif", sep = ".")
-writeRaster(altEden, raster_name)
 
-#------------------------------------------------------------------------------
-# READ IN USGS EDEN SURFACE INTERPOLATION & CREATE DIFFERENCES RASTER
-# AND ASSOCIATED SUMMARY DATA
+coordinates(wca2b_gages_anis) <- ~x_aniso + y_aniso
+proj4string(wca2b_gages_anis) <- nad_utm
+wca2b_rbf <- rbf(stage_cm ~ x_aniso + y_aniso, data = wca2b_gages_anis, func = "M", 
+                eta = 16.77, rho = 0, n.neigh = 8, newdata = wca2b_anis) 
+wca2b_rbf <- cbind(wca2b_coords, wca2b_rbf$var1.pred)
+colnames(wca2b_rbf)[3] <- "alt_stage_cm"
+head(wca2b_rbf)
 
-eden <- raster(eden_tiff)
+
+coordinates(wca3b_gages_anis) <- ~x_aniso + y_aniso
+proj4string(wca3b_gages_anis) <- nad_utm
+wca3b_rbf <- rbf(stage_cm ~ x_aniso + y_aniso, data = wca3b_gages_anis, func = "M", 
+                eta = 16.77, rho = 0, n.neigh = 8, newdata = wca3b_anis) 
+wca3b_rbf <- cbind(wca3b_coords, wca3b_rbf$var1.pred)
+colnames(wca3b_rbf)[3] <- "alt_stage_cm"
+head(wca3b_rbf)
+
+# note the number of neighbors here
+coordinates(pw_gages_anis) <- ~x_aniso + y_aniso
+proj4string(pw_gages_anis) <- nad_utm
+pw_rbf <- rbf(stage_cm ~ x_aniso + y_aniso, data = pw_gages_anis, func = "M", 
+                eta = 16.77, rho = 0, n.neigh = 5, newdata = pw_anis) 
+pw_rbf <- cbind(pw_coords, pw_rbf$var1.pred)
+colnames(pw_rbf)[3] <- "alt_stage_cm"
+head(pw_rbf)
+
+
+coordinates(other_gages_anis) <- ~x_aniso + y_aniso
+proj4string(other_gages_anis) <- nad_utm
+other_rbf <- rbf(stage_cm ~ x_aniso + y_aniso, data = other_gages_anis, func = "M", 
+                eta = 16.77, rho = 0, n.neigh = 8, newdata = other_anis) 
+other_rbf <- cbind(other_coords, other_rbf$var1.pred)
+colnames(other_rbf)[3] <- "alt_stage_cm"
+head(other_rbf)
+
+## combine together & convert to raster ----
+
+alt_eden <- rbind(wca1_rbf, wca2b_rbf, wca3b_rbf, pw_rbf, other_rbf)
+
+coordinates(alt_eden) <- ~X_COORD + Y_COORD
+proj4string(alt_eden) <- nad_utm
+gridded(alt_eden) <- TRUE
+alt_eden <- raster(alt_eden)
+plot(alt_eden)
+
+## compare with official EDEN surface  ----
+
+eden <- raster("E:/altEden/EDEN_surfaces/2008_q4_tiff_v2r1/s_20081001.tif")
 plot(eden)
 
-edens_diff <- altEden - eden
-plot(edens_diff)
-edens_diff
+# compare extent, grid
+alt_eden
+eden
 
-# summary stats
-cellStats(edens_diff, 'mean')
-cellStats(edens_diff, 'sd')
+eden_diff <- alt_eden - eden # predicted - actual
+plot(eden_diff)
 
-# export
-raster_name <- paste(rbf_output, "_diff", ".tif", sep = "")
-writeRaster(edens_diff, raster_name)
+# summary stats - useless but to compare against older output
+cellStats(eden_diff, 'mean')
+cellStats(eden_diff, 'sd')
 
+# find RMSE
+diff <- na.omit(getValues(eden_diff))
+sqrt(mean(diff^2))
 
-
+## export ----
+writeRaster(alt_eden, "/Volumes/external/altEden/R_run2/output/altEden_Trial1_20081001.tif")
+writeRaster(eden_diff, "/Volumes/external/altEden/R_run2/output/edenDiff_Trial1_20081001.tif")
