1ed368a06a9d9fd497d44d8fc5245f29de57fb7b
Saira Haider <shaider@usgs.gov>
2018-01-25 16:37:23 -0500

    2nd commit

--- Scripts/ever4cast_sims.R
+++ Scripts/ever4cast_sims.R
@@ -9,14 +9,21 @@
 # Romanach Lab @ Wetland and Aquatic Research Center 
 # US Geological Survey
 
-# Last update: January 2nd, 2017                      
-#------------------------------------------------------------------------------
+# Last update: January 4th, 2017                      
+#-----------------------------------------------------------------------------
 # LIBRARIES
 
 source("ever4cast_eden.R")
-source("netCDF_IO_v3.1.R")
+source("netCDF_IO_v3.1_varTYX.R")
 library(reshape2)
 library(data.table)
+
+#------------------------------------------------------------------------------
+# USER INPUTS
+
+sim_medians_folder <- "E:/ever4cast_2018/e4c_v1.1.2_forecast_stage_2018/spa_central_tendency_20180101_20180630/20180104_spa_central_tendency_20180101_20180630_daily_median_data"
+output_folder <- "E:/ever4cast_2018/eden_forecast2018/netcdfs/"
+output_csv_folder <- "E:/ever4cast_2018/eden_forecast2018/csvs/"
 
 #------------------------------------------------------------------------------
 # TO SPEED UP THE INTERPOLATION, SOME PARTS OF THE INTERPOLATION MUST BE DONE
@@ -88,7 +95,7 @@ colnames(wca3a_anis) <- c("x_aniso", "y_aniso")
 
 #------------------------------------------------------------------------------
 # Set up netcdf info 
-rotate <- function(x) t(apply(x, 2, rev))   # rotates matrix 90 degrees clockwise
+rotate <- function(x) t(apply(x, 2, rev)) # rotates matrix 90 degrees clockwise
 
 xDim            <- 287            #number of columns
 yDim            <- 405            #number of rows
@@ -100,7 +107,7 @@ out_layer       <- "depth"
 long_layer_name <- "Water Depth (cm)"
 out_units       <- "cm"
 out_prec        <- "float"
-background      <- -32768                                       
+background      <- NaN            ### JUST CHANGED THIS MIGHT NEED TO CHECK IT WORKS ####                             
 source_name    <- "interp_sim_depths.r"
 institution    <- "Wetland and Aquatic Research Center, USGS"
 qaqc           <- "under review"
@@ -109,17 +116,15 @@ comments       <- "Product derived from RBF interpolation of water stage over th
 #------------------------------------------------------------------------------
 # INTERPOLATION OF EVER4CAST SIMULATION DATA & OUTPUT NETCDF ------------------
 
-startTime <- proc.time()
-
 ## Get Data ## ----------------------------------------------------------------
 
 # DEM
 dem <- read.csv("./data/werp_dem_400m_cm.csv", stringsAsFactors = FALSE)
 
 # List of simulations
-sims <- list.files("./20171205_2017-spa-eden-analog-output_daily_median_data",
-                   full.names = TRUE)
-sim_names <- substr(sims, 58, 65)
+sims <- list.files(sim_medians_folder, full.names = TRUE)
+sims_filenames <- list.files(sim_medians_folder, full.names = FALSE)
+sim_names <- substr(sims_filenames, 1, 8)
 sim_list <- lapply(sims, fread)
 
 ## Set up time series ## ------------------------------------------------------
@@ -136,12 +141,17 @@ step_vec <- seq(1, tDim)
 time_steps <- sort(unique(sim_list[[1]]$Date))
 
 ## Run loop for each simulation, interpolate, and output netcdf file ## -------
-for(i in 1:length(sims)){              # i <- 1
+print(sims)
+
+startTime <- proc.time()
+
+for(i in 1:length(sims)){              # i <- 2
 
   df <- sim_list[[i]]
   daily_list <- split(df, df$Date) # list of daily dataframes
   
   # Run simulation for each day
+  print(paste("Simulation ", i, " of ", length(sims)))
   sim_stage_list <- lapply(daily_list, interpolate_gauges_csv)
 
   # Add column for date
@@ -154,8 +164,12 @@ for(i in 1:length(sims)){              # i <- 1
   sim_depth$depth <- sim_depth$stage - sim_depth$DEM_cm
   sim_depth <- within(sim_depth, rm(stage, DEM_cm))
   
-  # Write netcdf
-  output_file <- paste0("./output/", sim_names[i], "_depth", ".nc")
+  # Export depth as csv
+  output_csv_file <- paste0(output_csv_folder, sim_names[i], "_depth", ".csv")
+  fwrite(sim_depth, output_csv_file)
+  
+  # Write netcdf & export
+  output_file <- paste0(output_folder, sim_names[i], ".nc")
   
   nc_out <- createNetCDFfile(out.name = output_file,
                              layer.name = out_layer,
@@ -176,6 +190,8 @@ for(i in 1:length(sims)){              # i <- 1
   
   # Write data for each time step as a matrix to export as netcdf
   for(t in 1:tDim){                               # t <- 1
+    print(paste("Simulation ", i, " T-Step ", t, " of ", tDim))
+    
     # subset depth dataframe for each timestep
     dt <- as.data.frame(sim_depth[sim_depth$date == time_steps[t], ])
     # create matrix for netcdf
