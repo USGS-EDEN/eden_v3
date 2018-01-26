7b04d222c754531a3610da98ed342678f9573c63
Saira Haider <shaider@usgs.gov>
2018-01-26 12:58:51 -0500

    Clean up for re-do

--- Scripts/historical_surfaces.R
+++ Scripts/historical_surfaces.R
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------------------
 # This script runs the R version of the EDEN interpolation over the time period:
-# 2007 Quarter 2 - 2017 Quarter 2
+# 2007 Quarter 2 - 2017 Quarter 3
 
 # Output: 1) R interpolation as CSV
 #         2) R interpolation as raster
@@ -9,31 +9,22 @@
 #         5) Map of difference raster
 
 # Saira Haider 
+# shaider@usgs.gov
 # Romanach Lab @ Wetland and Aquatic Research Center 
 # US Geological Survey
-
-# Last update: January 12th, 2017                     
 #------------------------------------------------------------------------------
 
 source("eden_v3.R")
 
 #------------------------------------------------------------------------------
-
 # Read in gauge data
+
 medians1 <- list.files(path = "../gage_data", pattern = "median.txt", 
                       full.names = TRUE, recursive = TRUE)
 medians2 <- list.files(path = "../gage_data", pattern = "median_flag.txt", 
                        full.names = TRUE, recursive = TRUE)
 medians <- c(medians1, medians2)
 medians <- sort(medians)
-
-# --------------------------- JAN 5: ---------------------------------------------
-# READING IN GAUGE DATA THAT WAS CORRECTED BY BRYAN 
-# ORIGINALLY FILES WERE IMPROPERLY FORMATTED ONLINE
-# QUARTER 4 OF 2010 AND QUARTERS 1-3 OF 2011  HAD TO BE REDONE
-
-medians <- list.files("../gage_data/haider20180105b", pattern = "median_flag.txt", 
-                      full.names = TRUE, recursive = TRUE)
 
 #------------------------------------------------------------------------------
 
@@ -54,12 +45,6 @@ eden[1000:1990]
 eden[1991:2990]
 eden[2991:3836]
 
-# --------------------------- JAN 5TH ------------------------------------------
-## SUBSELECT FOR ONLY THOSE RASTERS THAT MATCH THE POORLY FORMATTED MEDIANS
-eden <- eden[1372:1736]
-#------------------------------------------------------------------------------
-
-
 #### RUN INTERPOLATION FUNCTIONS------------------------------------------------
 
 output_csvs <- "./output/historical_csvs/"
@@ -70,42 +55,8 @@ output_maps <- "./output/historical_diff_maps/"
 # vector to store RMSE
 error_vec <- vector()
 
+sink("console_print.txt")
 
-# --------------------------- JAN 5TH ------------------------------------------
-# THIS PROBLEM WAS FIXED
-    # From i = 1372 to i = 1736, there's a formatting problem in the daily medians file
-    # So have to fill that section with NA's
-    medians[1372]
-    medians[1736]
-    error_vec[1372:1736] <- NA
-    # Then keep running the function
-#------------------------------------------------------------------------------
-    
-# --------------------------- JAN 12TH ------------------------------------------
-## FIXED THIS PROBLEM ##
-    
-    
-# i = 1932 IS A BAD TIF FILE (20120413)
-error_vec[1932] <- NA
-
-# i = 2160 IS A BAD TIF FILE (20121127)
-error_vec[2160] <- NA
-
-# i = 3153 IS A BAD TIF FILE (20150817)
-error_vec[3153] <- NA
-
-# i = 3305 IS A BAD TIF FILE (20160116)
-error_vec[3305] <- NA
-
-# i = 3414 IS A BAD TIF FILE (20160504)
-error_vec[3414] <- NA
-
-# i = 3690 IS A BAD TIF FILE (20170204)
-error_vec[3690] <- NA
-#------------------------------------------------------------------------------
-
-
-sink("console_print_i1372_to_i1736.txt")
 for(i in 1:length(eden)){    #i <- 10
   
   df <- read.table(medians[i], sep = "\t", header = TRUE)
@@ -149,32 +100,11 @@ sink()
 
 
 ## Create RMSE dataframe & export
-rmse_df <- data.frame(cbind(median_dates[1:i], error_vec[1:i]), stringsAsFactors = FALSE)
+rmse_df <- data.frame(cbind(median_dates, error_vec), stringsAsFactors = FALSE)
 colnames(rmse_df) <- c("dates", "rmse_cm")
 rmse_df
 
-#write.csv(rmse_df, "./output/RMSE_alt_minus_real_BACKUP_20171219.csv")
 write.csv(rmse_df, "./output/RMSE_alt_minus_real.csv")
-
-# --------------------------- JAN 5  ------------------------------------------
-## READ IN OLD RMSE CSV, ADD NEW VALUES TO IT
-
-### SOMETHING IS WRONG WITH THE DAILY MEDIANS DATA FROM 09-30-2011
-
-rmse_csv <- read.csv("./output/RMSE_alt_minus_real.csv")
-rmse_csv[1372:1736, ]
-rmse_csv[1736, ]
-
-rmse_csv[1372:1735, ] <- error_vec
-mean(rmse_csv$rmse_cm , na.rm = TRUE)
-
-
-write.csv(rmse_csv, "./output/RMSE_alt_minus_real.csv", row.names = FALSE)
-
-#------------------------------------------------------------------------------
 
 mean(error_vec, na.rm = TRUE)
 summary(error_vec)
-
-# NOTES FOR EDEN TEAM:
-# PROBLEM WITH EDEN RASTER FROM 10-13-2016?
