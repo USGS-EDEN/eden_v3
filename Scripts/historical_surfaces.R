1333119a892e06574ae2dae31cd44335e883d37e
Saira Haider <shaider@usgs.gov>
2018-06-22 09:57:58 -0400

    Add date parameter

--- Scripts/historical_surfaces.R
+++ Scripts/historical_surfaces.R
@@ -19,6 +19,9 @@ gage_data <- "../EDEN/Data/gage_data"
 output_csvs <- "./Output/Stage/historical_csvs/"
 output_rasters <- "./Output/Stage/historical_rasters/"
 
+date1 <- "20170101"
+date2 <- "20171231"
+
 #------------------------------------------------------------------------------
 # Read in gauge data
 
@@ -28,19 +31,20 @@ medians2 <- list.files(path = gage_data, pattern = "median_flag.txt",
                        full.names = TRUE, recursive = TRUE)
 medians <- c(medians1, medians2)
 medians <- sort(medians)
+#medians[2000:2990]
 
 # Get dates
-median_dates  <- sub(".*/([0-9]+).*", "\\1", medians)
+median_dates  <- substr(basename(medians), 1, 8)
 median_dates
 median_dates[1000:1990]
 median_dates[1991:2990]
 median_dates[2991:3836]
 
-# Subset for 2007+ only
+# Subset for specified dates
 dates_num <- as.numeric(median_dates)
-median_dates <- median_dates[dates_num >= 20070101]
-medians <- medians[dates_num >= 20070101]
-medians[4000:4291]
+median_dates <- median_dates[dates_num >= date1 & dates_num <= date2]
+medians <- medians[dates_num >= date1 & dates_num <= date2]
+medians
 
 #------------------------------------------------------------------------------
 #### RUN INTERPOLATION FUNCTIONS 
@@ -48,7 +52,7 @@ medians[4000:4291]
 
 sink("./Output/historical_surfaces_consolePrint.txt")
 
-for(i in 1:length(medians)){    #i <- 1734
+for(i in 1:length(medians)){    #i <- 1
   
   df <- read.table(medians[i], sep = "\t", header = TRUE)
   print(medians[i])
@@ -57,15 +61,15 @@ for(i in 1:length(medians)){    #i <- 1734
   day <- median_dates[i]
   
   # interpolate & export
-  df <- interpolate_gauges(df)
-  csv_file <- paste0(output_csvs, "stage_cm_", day, ".csv")
-  write.csv(df, csv_file, row.names = FALSE)
+  df_raster <- interpolate_gages(df, format = "raster")
+  #csv_file <- paste0(output_csvs, "stage_cm_", day, ".csv")
+  #write.csv(df, csv_file, row.names = FALSE)
  
   # rasterize & export
-  coordinates(df) <- ~X_COORD + Y_COORD
-  proj4string(df) <- nad_utm
-  gridded(df) <- TRUE
-  df_raster <- raster(df)
+  # coordinates(df) <- ~X_COORD + Y_COORD
+  # proj4string(df) <- nad_utm
+  # gridded(df) <- TRUE
+  # df_raster <- raster(df)
   raster_file <- paste0(output_rasters, "stage_cm_", day, ".tif")
   writeRaster(df_raster, raster_file)
   
